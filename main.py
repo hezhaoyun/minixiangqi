0e9ba7a5b379899adaacc16c6d1d7f3ae5f9bb96
Zhaoyun <hezhaoyun@outlook.com>
2025-10-03 09:03:28 +0800

    add color pos on print_board

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from tools import print_board_text
 
 def main():
     """主函数, 模拟对局"""
+
+    SEARCH_DEPTH = 5
+    SIMULATE_STEPS = 24
+
     game_board = Board()
     engine = Engine()
 
     print_board_text(game_board)
 
-    for i in range(24):
-        final_score, best_move = engine.search(game_board, 5)
+    for i in range(SIMULATE_STEPS):
+        final_score, best_move = engine.search(game_board, SEARCH_DEPTH)
         print(f"\n评估分数 (从当前玩家角度): {final_score}，最佳着法是: {best_move}")
 
         if best_move is None:
